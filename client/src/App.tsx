f32a709f01ed31f5adf67d1deb99d3371e845d09
lad94220 <laduy231@clc.fitus.edu.vn>
2025-12-15 22:08:45 +0700

    add cover iamge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
+import { Header, Input } from "./components"
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="h-dvh">
+      <div className="z-auto">
+        <img 
+          src="public/cover.jpg" 
+          className="h-83 w-full object-cover object-[50%_83%] absolute -z-10 opacity-95"
+        />
+        <Header />
+        <Input />
+      </div>
+    </div>
   )
 }
 
