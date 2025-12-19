1b31bacbb93edc00bdd63e9ae7303f69f2e3dafd
lad94220 <laduy231@clc.fitus.edu.vn>
2025-12-19 12:35:27 +0700

    add header

--- app/client/src/components/Header.tsx
+++ app/client/src/components/Header.tsx
@@ -1,9 +1,17 @@
+import type { POIResponse } from "../types";
 
-export const Header = () => {
+interface HeaderProps {
+  setResult: (result: POIResponse) => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({ setResult }) => {
 
   return (
     <div className="h-18 bg-transparent">
-      <div className="max-w-5xl mx-auto h-full flex items-center text-white font-bold text-2xl">
+      <div 
+        className="max-w-5xl mx-auto h-full flex items-center text-white font-bold text-2xl cursor-pointer"
+        onClick={() => setResult(null!)}  
+      >
         Tour Planner
       </div>
     </div>
